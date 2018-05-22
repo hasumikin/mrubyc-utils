d5774d66162d9d6684d70725cef46893e1ff4184
HASUMI Hitoshi <hitoshi.hasumi@monstar-lab.com>
2018-05-22 20:38:55 +0900

    feature install

---
+++ mrblib/mrubyc-utils/common.rb
@@ -0,0 +1,46 @@
+module MrubycUtils
+  class << self
+    CONFIG_FILE = '.mrubycconfig'
+
+    def cp(from, to)
+      unless File.exists?(from)
+        puts "WARN - skippeng copy because '#{from}' does not exist"
+        return false
+      end
+      puts "WARN - '#{to}' will be overwritten" if File.exists?(to)
+      File.open(from) do |f_from|
+        File.open(to, 'w') do |f_to|
+          f_to.puts f_from.read
+        end
+      end
+      puts "INFO - copied from '#{from}' to '#{to}'"
+    end
+
+    def mkdir(dir)
+      if !File.directory?(dir)
+        if !Dir.mkdir(dir)
+          puts "FATAL - failed to create directory: #{dir}"
+          raise RuntimeError
+        end
+      end
+      puts "INFO - created '#{dir}/'"
+    end
+
+    def save_config(config)
+      File.open(CONFIG_FILE, 'w') do |f|
+        f.puts YAML.dump(config)
+      end
+    end
+
+    def load_config
+      unless File.exists?(CONFIG_FILE)
+        puts "FATAL - #{CONFIG_FILE} is not found. You have to `mrubyc-utils install` first or confirm your current directory"
+        return false
+      end
+      File.open(CONFIG_FILE) do |f|
+        YAML.load(f.read)
+      end
+    end
+
+  end
+end
---
+++ mrblib/mrubyc-utils/install.rb
@@ -0,0 +1,114 @@
+module MrubycUtils
+  class << self
+    CONFIGURATIONS = {
+      target: {
+        question: 'target microcontroller',
+        default: 'psoc5lp'
+      },
+      mrubyc_dir: {
+        question: 'dir name of mruby/c repository',
+        default: 'mrubyc'
+      },
+      mruby_lib_dir: {
+        question: 'dir name where your mruby code are lacated',
+        default: 'mrbsrc'
+      },
+      c_lib_dir: {
+        question: 'dir name where *.c sources are located',
+        default: 'src'
+      },
+      prefix: {
+        question: 'prefix of your mruby source file name',
+        default: 'job'
+      }
+    }
+
+    def install
+      config = {}
+      CONFIGURATIONS.each do |key, value|
+        print "#{value[:question]} [#{value[:default]}]: "
+        config[key] = gets.chomp
+        config[key] = value[:default] if config[key].empty?
+        puts ''
+      end
+      save_config(config)
+      git_clone_mrubyc(config)
+      create_c_lib_dir(config)
+      copy_mrubyc_to_src(config)
+      create_mruby_lib_dir(config)
+      download_templates(config)
+      add_gitignore(config)
+    end
+
+    def git_clone_mrubyc(config)
+      `git clone https://github.com/mrubyc/mrubyc.git #{config[:mrubyc_dir]}`
+    end
+
+    def copy_mrubyc_to_src(config)
+      src = "#{config[:mrubyc_dir]}/src/"
+      Dir.foreach(src) do |filename|
+        if ['.h', '.c'].include?(File.extname(filename))
+          to = "#{config[:c_lib_dir]}/#{filename}"
+          cp("#{src}/#{filename}", to)
+        end
+      end
+      mkdir("#{config[:c_lib_dir]}/hal")
+      ['h', 'c'].each do |ext|
+        cp("#{config[:mrubyc_dir]}/src/hal_#{config[:target]}/hal.#{ext}", "#{config[:c_lib_dir]}/hal/hal.#{ext}")
+      end
+    end
+
+    def create_c_lib_dir(config)
+      mkdir(config[:c_lib_dir])
+    end
+
+    def create_mruby_lib_dir(config)
+      mkdir(config[:mruby_lib_dir])
+    end
+
+    def download_templates(config)
+      mruby_lib_dir = config[:mruby_lib_dir]
+      prefix = config[:prefix]
+      http = HttpRequest.new()
+      [ { from: "targets/#{config[:target]}/main.c.erb", to: 'main.c' },
+        { from: 'mrblib/prefix_main_loop.rb.erb', to: "#{mruby_lib_dir}/#{prefix}_main_loop.rb" },
+        { from: 'mrblib/prefix_sub_loop.rb.erb', to: "#{mruby_lib_dir}/#{prefix}_sub_loop.rb" },
+        { from: 'mrblib/prefix_operations.rb.erb', to: "#{mruby_lib_dir}/#{prefix}_operations.rb" }
+      ].each do |template|
+        url = sprintf("https://%s/%s/%s", 'raw.githubusercontent.com', 'hasumon/mrubyc-utils/master/templates', template[:from])
+        puts "INFO - download #{url}"
+        request = http.get(url, {})
+        if request.code != 200
+          puts "FATAL - template file '#{url}' was not found"
+          raise RuntimeError
+        end
+        erb = if File.extname(template[:to]) == '.c'
+          request.body.gsub(/\n/, "\r\n")
+        else
+          request.body
+        end
+        File.open(template[:to], 'w') do |f|
+          f.puts erb_result(erb, config)
+        end
+        puts "INFO - saved #{template[:to]}"
+      end
+    end
+
+    def erb_result(erb, hash)
+      hash.each do |key, value|
+        erb.gsub!("<%= #{key} %>", value)
+      end
+      erb
+    end
+
+    def add_gitignore(config)
+      File.open('.gitignore', 'a') do |f|
+        ['# added by mrubyc-utils', "#{config[:mrubyc_dir]}/"].each do |line|
+          f.puts line
+          puts "INFO - \"#{line}\" was added to .gitignore"
+        end
+      end
+    end
+
+  end
+end
