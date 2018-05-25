f6abf3af2c0cbefec68e15627ad18ee808f6d7dc
HASUMI Hitoshi <hitoshi.hasumi@monstar-lab.com>
2018-05-25 17:12:21 +0900

    write usage of tag and checkout

--- mrblib/mrubyc-utils/usage.rb
+++ mrblib/mrubyc-utils/usage.rb
@@ -4,10 +4,14 @@ module MrubycUtils
       msg =<<EOS
 Usage: mrubyc-utils COMMAND [ARGS]
 
- install        Install mruby/c and setup some templates.
+ install        Install mruby/c repo into your local and setup templates.
                 Please run this command at the top directory
-                of your project (it shoud have 'main.c').
- update         Update mruby/c to the newest master branch.
+                of your project (normally it should have 'main.c').
+ update         Update mruby/c repo to the newest master branch.
+ checkout       Checkout specified tag or commit of mruby/c repo.
+  -t | --tag      [required] You can specify anything that
+                  `git checkout` will accept.
+ tag            Show all tags of mruby/c repogitory that you installed.
  classes        Show all the classes that are defined in
                 mruby/c's virtual machine.
  methods        Show all the methods that are available
